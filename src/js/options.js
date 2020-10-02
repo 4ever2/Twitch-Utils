08c2e65aaa1d250a97ee3e6de6ba7b673a1e66a8
Eske <eskehoy@gmail.com>
2020-10-03 00:56:59 +0200

    Add hide ads option

--- src/js/options.js
+++ src/js/options.js
@@ -2,9 +2,11 @@
 function save_options() {
   var frontPageAutoPlay = document.getElementById('frontPageAutoPlay').checked;
   var adMute = document.getElementById('adMute').checked;
+  var adHide = document.getElementById('adHide').checked;
   chrome.storage.local.set({
     frontPageAutoPlay: frontPageAutoPlay,
-    adMute: adMute
+    adMute: adMute,
+    adHide: adHide
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -21,10 +23,12 @@ function restore_options() {
   // Use default value frontPageAutoPlay = true.
   chrome.storage.local.get({
     frontPageAutoPlay: true,
-    adMute: true
+    adMute: true,
+    adHide: true
   }, function(items) {
     document.getElementById('frontPageAutoPlay').checked = items.frontPageAutoPlay;
     document.getElementById('adMute').checked = items.adMute;
+    document.getElementById('adHide').checked = items.adHide;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
